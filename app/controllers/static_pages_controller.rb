c12bf7bf51952925f02c4e3655d061611cee30df
julio <julio336@hotmail.com>
2013-03-06 23:32:07 -0600

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def menu
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jhoma::Application.config.secret_token = '10c6a4a6c8f91b9e854c2d120258eec4fd4c6fee34998cca1d9f652114187921710b048abb54e0a5bd9d82684f362d1ad088214ede3cec39fd47761d62b0bcd3'
